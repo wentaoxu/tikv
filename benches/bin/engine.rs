46f968a190a47f40113247df06b98280d980475a
XuWentao <wtao.xu@gmail.com>
2017-12-12 13:33:26 +0800

    fix format

--- benches/bin/engine.rs
+++ benches/bin/engine.rs
@@ -1,37 +1,33 @@
 use std::sync::mpsc::{channel, Sender};
-use tikv::storage::{Config, Storage, ALL_CFS, new_local_engine};
+use tikv::storage::{new_local_engine, Config, Storage, ALL_CFS};
 use tempdir::TempDir;
 
-fn bench_get(){
+fn bench_get() {
     let dir = TempDir::new("engine_bench").unwrap();
     let e = new_local_engine(dir.path().to_str().unwrap(), ALL_CFS).unwrap();
 
 }
 
-fn bench_load(){
+fn bench_load() {
     let dir = TempDir::new("engine_bench").unwrap();
     let e = new_local_engine(dir.path().to_str().unwrap(), ALL_CFS).unwrap();
 }
 
-fn bench_update(){
+fn bench_update() {
     let dir = TempDir::new("engine_bench").unwrap();
     let e = new_local_engine(dir.path().to_str().unwrap(), ALL_CFS).unwrap();
 }
 
-fn bench_insert(){
+fn bench_insert() {
     let dir = TempDir::new("engine_bench").unwrap();
     let e = new_local_engine(dir.path().to_str().unwrap(), ALL_CFS).unwrap();
 }
 
-fn new_write_key() -> str {
+fn new_write_key() -> str {}
 
-}
-
-fn new_lock_key() -> str {
-
-}
+fn new_lock_key() -> str {}
 
 pub fn bench_engine() {
     printf!("benching tombstone scan with rocksdb\t...\t");
-//    print_result(bench_tombstone_scan());
+    //    print_result(bench_tombstone_scan());
 }
